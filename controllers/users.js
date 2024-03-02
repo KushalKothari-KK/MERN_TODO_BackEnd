30f114c92c66101c893fd4aa2f0e52355e37dc7d
KushalKothari-KK <kushalvkotharikvk@gmail.com>
2024-03-02 19:59:22 +0530

    Cookie updates done

--- controllers/users.js
+++ controllers/users.js
@@ -1,6 +1,7 @@
 const bcrypt = require("bcrypt");
 const User = require("../models/user");
 const { ErrorHandler } = require("../middlewares/error");
+const { sendCookie } = require("../utils/feature.js");
 
 const registerUser = async (req, res, next) => {
   try {
@@ -9,11 +10,7 @@ const registerUser = async (req, res, next) => {
     if (user) return next(new ErrorHandler("User Already Exist", 404));
     const hashedPassword = await bcrypt.hash(password, 10);
     user = await User.create({ name, email, password: hashedPassword });
-    // need to add cookie logic and remove below code
-    res.status(201).json({
-      success: true,
-      message: "User Registered Successfully",
-    });
+    sendCookie(user, res, "Registered Successfully", 201);
   } catch (err) {
     next(err);
   }
@@ -28,6 +25,7 @@ const loginUser = async (req, res, next) => {
     const isMatched = await bcrypt.compare(password, user.password);
     if (!isMatched)
       return next(new ErrorHandler("Invalid username or password", 404));
+    sendCookie(user, res, `Welcome back ${user.name}`, 200);
     res.status(200).json({
       success: true,
       message: "Login Successfully",
@@ -37,7 +35,23 @@ const loginUser = async (req, res, next) => {
   }
 };
 
-const logoutUser = async (req, res, next) => {};
+const logoutUser = async (req, res, next) => {
+  try {
+    res
+      .status(200)
+      .cookie("token", "", {
+        expires: new Date(Date.now()),
+        sameSite: process.env.NODE_ENV === "Developement" ? "lax" : "none",
+        secure: process.env.NODE_ENV === "Developement" ? false : true,
+      })
+      .json({
+        success: true,
+        user: req.user,
+      });
+  } catch (err) {
+    next(err);
+  }
+};
 
 const getUserInfo = async (req, res, next) => {
   try {
